dab162c7e108a38016232618256f4c4939e355e8
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2018-10-22 11:36:47 +0300

    create meetup sty;les fixed

--- meetup-project/src/components/meetup/CreateMeetup.vue
+++ meetup-project/src/components/meetup/CreateMeetup.vue
@@ -1,15 +1,14 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-        <h4>Create a new Meetup</h4>
-      </v-flex>
     </v-layout>
     <v-layout row>
-      <v-flex xs12>
+      <v-flex xs12 sm10 md10 lg8 offset-sm1 offset-md1 offset-lg2>
+        <v-card class="create-meetup-form">
+        <v-card-text>
         <form @submit.prevent="onCreateMeetup">
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <v-text-field
                 name="title"
                 label="Title"
@@ -20,7 +19,7 @@
             </v-flex>
           </v-layout>
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <v-text-field
                 name="location"
                 label="Location"
@@ -30,7 +29,7 @@
             </v-flex>
           </v-layout>
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <v-text-field
                 name="imageUrl"
                 label="Image URL"
@@ -40,12 +39,12 @@
             </v-flex>
           </v-layout>
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <img :src="imageUrl" height="150">
             </v-flex>
           </v-layout>
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <v-text-field
                 name="description"
                 label="Description"
@@ -55,36 +54,37 @@
                 required></v-text-field>
             </v-flex>
           </v-layout>
-          <v-layout row>
-            <v-flex xs12 sm6>
-              <h4>Choose a date</h4>
+          <v-layout row class="data-time-picker-container">
+            <v-flex xs12 class="data-picker-holder">
                <v-date-picker 
                   v-model="date"
                   color="green lighten-1" 
                   header-color="primary"
+                  class="centered-data-time-picker"
                ></v-date-picker>
-               <p>{{date}}</p>
             </v-flex>
-            <v-flex xs12 sm6>
-              <h4>Choose time</h4>
+            <v-flex xs12 class="time-picker-holder">
                <v-time-picker 
                   v-model="time" 
                   format = "24hr"
                   color="green lighten-1" 
                   header-color="primary"
+                  class="centered-data-time-picker"
                ></v-time-picker>
-               <p>{{time}}</p>
             </v-flex>
           </v-layout>
           <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12>
               <v-btn
-                class="primary"
+                class="primary submit-from-button-centered"
                 :disabled="!formIsValid"
-                type="submit">Create Meetup</v-btn>
+                type="submit"
+                >Create Meetup</v-btn>
             </v-flex>
           </v-layout>
         </form>
+        </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -148,3 +148,49 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-picker.v-card.v-picker--date.centered-data-time-picker.theme--light {
+  margin: auto;
+  width: 290px;
+  display: flex;
+}
+.v-picker.v-card.v-picker--date.centered-data-time-picker.theme--light
+  .v-picker__title {
+  height: 101px;
+}
+
+.v-picker.v-card.v-picker--time.centered-data-time-picker.theme--light {
+  margin: auto;
+  width: 290px;
+  display: flex;
+}
+
+.v-date-picker-table {
+  height: 246px;
+}
+.create-meetup-form {
+  margin-top: 10px;
+}
+
+.submit-from-button-centered {
+  margin: 20px auto 0;
+  display: block;
+}
+
+@media screen and (max-width: 776px) {
+  .data-time-picker-container {
+    flex-direction: column !important;
+  }
+  .centered-data-time-picker {
+    margin-top: 15px !important;
+  }
+}
+
+@media screen and (max-width: 322px) {
+  .v-picker.v-card.v-picker--time.centered-data-time-picker.theme--light,
+  .v-picker.v-card.v-picker--date.centered-data-time-picker.theme--light {
+    margin-left: -17px;
+  }
+}
+</style>
